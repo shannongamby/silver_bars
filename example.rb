b34d076304bf29d5b503b4ff70eea53c272b5090
Shannon Gamby <shannongamby@hotmail.com>
2019-01-20 23:26:28 +0000

    added screent shots

--- example.rb
+++ example.rb
@@ -1,27 +1,28 @@
 require './lib/order_board.rb'
 
-new = OrderBoard.new
+client = OrderBoard.new
 
-new.place_order(1, 1, 1, :BUY)
-new.place_order(1, 1, 1, :BUY)
-new.place_order(1, 1, 10, :BUY)
-new.place_order(1, 1, 100, :BUY)
+client.place_order(1, 1, 1, :BUY)
+client.place_order(1, 1, 1, :BUY)
+client.place_order(1, 1, 10, :BUY)
+client.place_order(1, 1, 100, :BUY)
 
-new.place_order(1, 1, 1, :SELL)
-new.place_order(1, 1, 10, :SELL)
-new.place_order(1, 1, 5, :SELL)
-new.place_order(1, 1, 5, :SELL)
+client.place_order(1, 1, 1, :SELL)
+client.place_order(1, 1, 10, :SELL)
+client.place_order(1, 1, 5, :SELL)
+client.place_order(1, 1, 5, :SELL)
 
 puts "\n8 orders have been placed: 4 buy orders and 4 sell orders"
 puts "2 buy orders have been aggregated to a quantity of 2kg"
 puts "2 sell orders have been aggregated to a quantity of 2kg"
+
 puts "\nHere is the order board:\n"
-new.print_order_board
+client.print_order_board
 
 puts "\nAnd when we remove a buy order:\n"
-new.remove_order(4)
-new.print_order_board
+client.remove_order(4)
+client.print_order_board
 
 puts "\nAnd when we remove a sell order that has been aggregated:\n"
-new.remove_order(8)
-new.print_order_board
+client.remove_order(8)
+client.print_order_board
